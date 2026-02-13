2ad7ecf9f73b837a220908de528d0d7c2644f065
Chalindu Kodikara <chalindumkodikara@gmail.com>
2026-02-13 20:12:49 +0530

    Improve routing

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/valentine-website">
         <Navbar />
         <main>
           <Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/valentine-s-loveletters/',
+  base: '/valentine-website/',
   server: {
     host: "::",
     port: 8080,
